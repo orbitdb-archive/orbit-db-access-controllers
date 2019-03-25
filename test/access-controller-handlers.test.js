e562feb50f6032e3b7b0b3333a7261b2f521bb2a
shamb0t <shams.methnani@gmail.com>
2019-03-25 13:15:13 +0000

    Decompress key before canAppend
    Update Keystore.create calls

--- test/access-controller-handlers.test.js
+++ test/access-controller-handlers.test.js
@@ -43,8 +43,8 @@ Object.keys(testAPIs).forEach(API => {
       ipfs1 = ipfsd1.api
       ipfs2 = ipfsd2.api
 
-      const keystore1 = Keystore.create(dbPath1 + '/keys')
-      const keystore2 = Keystore.create(dbPath2 + '/keys')
+      const keystore1 = await Keystore.create(dbPath1 + '/keys')
+      const keystore2 = await Keystore.create(dbPath2 + '/keys')
 
       id1 = await IdentityProvider.createIdentity({ id: 'A', keystore: keystore1 })
       id2 = await IdentityProvider.createIdentity({ id: 'B', keystore: keystore2 })
